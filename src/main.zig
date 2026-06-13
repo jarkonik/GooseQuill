7ba82664aff5f71fe1ea0c45a26e2507b5d865e3
ape-sudo <jakuboczkowski2@gmail.com>
2026-06-13 20:13:01 +0200

    Cleanup build

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,22 @@
 const std = @import("std");
 const Io = std.Io;
 
+const sdl = @cImport(@cInclude("SDL2/SDL.h"));
+
 const gq = @import("GooseQuill");
 
 pub fn main() void {
     gq.hello_world();
+
+    const WIDTH = 800;
+    const HEIGHT = 600;
+
+    sdl.SDL_Create_Window(
+        "GooseQuill",
+        sdl.SDL_WINDOWPOS_CENTERED,
+        sdl.SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        sdl.SDL_WINDOW_SHOWN,
+    );
 }
